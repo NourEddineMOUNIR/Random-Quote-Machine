6985f058363bc4479141dfd5c62a51e20712afa8
nour eddine mounir <noureddine.mounir94@gmail.com>
2018-03-02 20:24:19 +0000

    etablir la communication avec l'api des quotes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
-/**
- * Created by Root on 2/28/2018.
- */
+
+// finish the designe
+// sherch for an quotes api
+// add event listner to my button
+// send a request to the api
+// change the DOM
+$(document).ready(function () {
+    // disable cache in all future ajax
+    $.ajaxSetup({ cache: false });
+
+    // get references
+    var quoteContent = $("#quoteContent");
+    var quoteAuthor = $("#quoteAuthor");
+
+    var getQuote = function () {
+        $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=2", function (resp) {
+            quoteAuthor.hide();
+            quoteContent.hide();
+            quoteAuthor.html(resp[0].title);
+            quoteContent.html(resp[0].content);
+            quoteAuthor.fadeIn("slow");
+            quoteContent.fadeIn("slow");
+        });
+    };
+
+    var setIntervalVar;
+    var setIntervalQuote = function () {
+        return setInterval(function () {
+            getQuote();
+        }, 10000);
+    };
+
+    $("#getQuoteButton").on('click', function () {
+        if(setIntervalVar) clearInterval(setIntervalVar);
+        getQuote();
+        setIntervalVar = setIntervalQuote();
+    });
+
+    setIntervalVar = setIntervalQuote();
+
+});
